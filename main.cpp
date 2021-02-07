40ff1c9bff3afb24bd1f77a907b61e4dd5bf549c
Klaus <Klaus.Kleiner@gmx.net>
2021-02-07 16:34:52 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "utils.h"
+
+int main()
+{
+    int a = 2;
+    int b = 3;
+    int c = addInt(a, b);
+    
+    std::cout << "a is " << a << ", b is " << b << ", the sum c is " << c << std::endl;
+
+}
